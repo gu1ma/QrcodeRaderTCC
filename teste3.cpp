10d47d6ff083da527984126800f23ef5fdd5b8e5
Gabriel Guimarães <gabrielgs1408@gmail.com>
2017-10-22 21:07:56 -0200

    [Add] Alteracoes diversas
    Foram efetuadas algumas alteracoes no codigo como por exemplo o aumento no tamanho da resolucao do qrcode

--- teste3.cpp
+++ teste3.cpp
@@ -21,8 +21,8 @@ using namespace std;
 using namespace zbar;  
 using namespace cv; 
 
-void generateKeys(string keyName){
-    string genPrivKey = "openssl genrsa -out keys/"+ keyName +".pri 2048";
+void generateKeys(string keyName, string keyLenght){
+    string genPrivKey = "openssl genrsa -out keys/"+ keyName +".pri "+keyLenght;
     string genPubKey = "openssl rsa -in keys/"+ keyName +".pri -pubout > keys/"+ keyName +".pub";
     string fileName = "keys/" + keyName + ".pri";
     ifstream file;
@@ -35,14 +35,30 @@ void generateKeys(string keyName){
     file.close();
 }
 
-void encryptFile(string fileName, string newFileName, string keyName){
+//Encriptando dados com RSA
+void encryptFileWithRsa(string fileName, string newFileName, string keyName){
     string encrypt = "openssl rsautl -encrypt -inkey keys/"+ keyName +".pub -pubin -in "+ fileName +" -out encBinary.txt";
     system(encrypt.c_str());
     string encryptB64= "base64 encBinary.txt >"+newFileName;
     system(encryptB64.c_str());
 }
 
-void decryptFile(string fileName, string newFileName, string keyName){
+//Encriptando dados com AES
+void encryptFileWithAES(string fileName, string newFileName, string keyFile){
+    string encrypt = "openssl aes-256-cbc -in " +fileName+ " -a -out "+newFileName+" -k keys/"+keyFile;
+    system(encrypt.c_str());
+}
+
+//Decriptando dados com AES
+void decryptFileWithAES(string fileName, string newFileName, string keyFile){
+    string decrypt = 
+    "openssl aes-256-cbc -d -a -in "+fileName+" -out "+newFileName+" -k keys/"+keyFile;
+    system(decrypt.c_str());
+    //cout << decrypt << endl;
+}
+
+//Decriptando dados com RSA
+void decryptFileWithRsa(string fileName, string newFileName, string keyName){
     ifstream file;
     file.open(newFileName.c_str());
     //Se não existir o arquivo decriptografado, geramos ...
@@ -73,18 +89,15 @@ void saveDataInFile(const char *fileName, string data){
     } else {
         cout << "Problemas ao abrir o arquivo";
     }
-
 }
 
 void generateQrCode(string qrCodeName, string fileName){
     string data = getDataFromFile(fileName.c_str());
-    //data = "\"" + data +"\"";
     string generateQrCode = "qrencode \"" + data + "\" -o qrCodes/"+qrCodeName+".png";
-    //cout << generateQrCode << endl;
     system(generateQrCode.c_str());
 }
 
-int readQrCode(int argc, char *argv[], string fileName, string newFileName,string keyName){
+int readQrCode(int argc, char *argv[], string fileName, string newFileName,string keyName, bool aes){
     int exit = 1;
     ImageScanner scanner;
     Mat img;
@@ -94,7 +107,7 @@ int readQrCode(int argc, char *argv[], string fileName, string newFileName,strin
     scanner.set_config(ZBAR_NONE, ZBAR_CFG_ENABLE, 1);
     img = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
     string data;
-    VideoCapture cap(0);
+    VideoCapture cap(1);
     if (!cap.isOpened()){  
         cout << "Cannot open the video cam" << endl;  
         return -1;  
@@ -105,15 +118,16 @@ int readQrCode(int argc, char *argv[], string fileName, string newFileName,strin
     namedWindow("Leitor", CV_WINDOW_AUTOSIZE);
     while(exit){
         bool bSucess = cap.read(img);
-        cv::cvtColor(img, grayImg, cv::COLOR_BGR2GRAY);
+        cvtColor(img, grayImg, cv::COLOR_BGR2GRAY);
+        //grayImg = img;
         int width = grayImg.cols;
         int height = grayImg.rows;
         uchar *raw = (uchar *) grayImg.data;
-
+        //Paramethers width, heigth, format, data, length
         Image image(width, height, "Y800", raw, width * height);
-        //scan image
+        //Scan image
         int n = scanner.scan(image);
-        //get results
+        //Get results
         for(Image::SymbolIterator symbol = image.symbol_begin();
             symbol != image.symbol_end(); 
             ++symbol
@@ -123,12 +137,16 @@ int readQrCode(int argc, char *argv[], string fileName, string newFileName,strin
       
             //cout << "Tipo: " << typeid(symbol->get_data()).name() << endl;
       
-            //cout << "Dados criptografados: " << symbol->get_data() << "" << endl;
+            cout << "Dados criptografados: " << symbol->get_data() << "" << endl;
 
             data = symbol->get_data();
 
             saveDataInFile(fileName.c_str(), data);            
-            decryptFile(fileName, newFileName, keyName);
+            if(aes){
+                decryptFileWithAES(fileName, newFileName, keyName);
+            } else {
+                decryptFileWithRsa(fileName, newFileName, keyName);
+            }
 
             //Cria retangulo ao redor do QRCode 
             int n = symbol->get_location_size();
@@ -142,8 +160,6 @@ int readQrCode(int argc, char *argv[], string fileName, string newFileName,strin
             for(int i = 0;i < 4;i++){   
                 line(grayImg,pts[i],pts[(i+1)%4],Scalar(255,0,0),3);   
             }
-            //break;
-            //exit = 0;
         }
         imshow("Leitor", grayImg);
         if (waitKey(30) == 27){  
@@ -154,13 +170,12 @@ int readQrCode(int argc, char *argv[], string fileName, string newFileName,strin
 }
 
 int main(int argc, char *argv[]){
-    generateKeys("camera");
-    //generateQrCode("qrCode1", "file-example-enc.txt");
-    //encryptFile("file-example.txt", "file-example-enc.txt", "camera");
-    //decryptFile("file-example-enc.txt", "file-example-dec.txt", "camera");
-    //generateQrCode("qrCode1", "file-example-enc.txt");
-    //readQrCode(argc, argv, "file-example-enc.txt", "file-example-dec.txt", "camera");
-    //
+    //Lendo o QRCode caso a criptografia seja AES
+    //Parametros: argc, argv, nome do arquivo a ser gerado encriptado, nome do arquivo apos decriptacao,
+    //nome da chave a ser usada na decriptacao e bool com true para AES e false para RSA
+    //readQrCode(argc, argv, "file-example-enc.txt", "file-example-dec.txt", "aesPass.txt", true);
+    encryptFileWithAES("file-example.txt", "f-enc.txt", "aesPass.txt");
+    generateQrCode("smallQrCode", "f-enc.txt");
 
 	return 0;
 }
